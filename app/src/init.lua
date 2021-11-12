e83a5e18c2ac345829bbc7bb83dd4ddf56174d34
Goruti <diego_antonino@hotmail.com>
2021-11-12 10:08:20 -0500

    v0.0.1

--- app/src/init.lua
+++ app/src/init.lua
@@ -28,36 +28,12 @@ gpio.config({gpio={RED_LED, BLUE_LED}, dir=gpio.OUT})
 --server_start()
 
 ----------------------------
-    -- tsl2561
---tsl2561 = require("tsl2561")
---tsl2561.init(SDA_PIN, SCL_PIN)
---lux = tsl2561.readVisibleLux()
---
---    -- release module
---tsl2561 = nil
---package.loaded["tsl2561"]=nil
-
-id=i2c.SW
-LUX_ADDR=0x39
-REGISTER = 0x00
-VALUE = 0x03
-i2c.setup(id, SDA_PIN, SCL_PIN, i2c.SLOW)
-
--- attempt to read chip id and compare against expected value
-function simple_check_chip(dev_address, dev_register, value)
-i2c.start(id)
---assert(i2c.address(id, dev_address, i2c.TRANSMITTER) , "!!i2c device did not ACK first address operation" )
-i2c.address(id, dev_address, i2c.TRANSMITTER)
-i2c.write(id, dev_register)
-i2c.write(id, value)
-
-i2c.start(id) -- repeated start condition
---assert( i2c.address(id, dev_address, i2c.RECEIVER) , "!!i2c device did not ACK second address operation" )
-i2c.address(id, dev_address, i2c.RECEIVER)
-print("REGISTER ", dev_register, " Value: ", i2c.read(id, 1):byte())
-i2c.stop(id)
-end
---
-
-print("Chip check, should succeed if chip is present and functional")
-simple_check_chip(LUX_ADDR, REGISTER, VALUE)
+    -- tsl2591
+--tsl2591 = require("tsl2591")
+--tsl2591.init(ID, SDA_PIN, SCL_PIN, i2c.SLOW)
+-- set gain and integration time
+-- see tsl2591.lua for a list of options
+--tsl2591.set_gain(TSL2591_GAIN_25X, TSL2591_INT_TIME_600MS)
+--vis, ir = tsl2591.read()
+--print("CH0 (visible):", vis)
+--print("CH1 (IR):", ir)
